d149a293b757ea1720a2fd4717f35360066565cc
IsaacVilla11 <124637331+IsaacVilla11@users.noreply.github.com>
2025-01-15 00:19:56 -0500

    Login
    Login para google en movil y web

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 // screens
 import 'package:flutter_application_1/screens/home.dart';
 import 'package:flutter_application_1/screens/profile.dart';
@@ -8,81 +10,125 @@ import 'package:flutter_application_1/screens/settings.dart';
 import 'package:flutter_application_1/screens/components.dart';
 import 'package:flutter_application_1/screens/onboarding.dart';
 import 'package:flutter_application_1/screens/signIn.dart';
-// vistas navegacion
 import 'package:flutter_application_1/screens/Camera.dart';
 import 'package:flutter_application_1/screens/Historial.dart';
 import 'package:flutter_application_1/screens/crearCuenta.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    print("Error inicializando Firebase: $e");
+  }
   runApp(MaterialKitPROFlutter());
+}   
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return const FirebaseOptions(
+          apiKey: "AIzaSyBxRSkFdRBltJk0dVGrjC81Ep8Iu6FX8uA",
+          authDomain: "signspeak-ai.firebaseapp.com",
+          projectId: "signspeak-ai",
+          storageBucket: "signspeak-ai.firebasestorage.app",
+          messagingSenderId: "105200073019",
+          appId: "1:105200073019:web:8547b604c1357ce4d35f1e");
+    }
+    return const FirebaseOptions(
+      apiKey: "AIzaSyA_DjIChFuhrNY7hmp97Tnvt3liig3377U",
+      appId: "1:105200073019:android:52d592e95936556ad35f1e",
+      messagingSenderId: "105200073019",
+      projectId: "signspeak-ai",
+      storageBucket: "signspeak-ai.firebasestorage.app",
+      androidClientId: "105200073019-41br8gloqnrtp0l9fk7tecl4e1qfrvmd.apps.googleusercontent.com",
+    );
+  }
 }
 
 class MaterialKitPROFlutter extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Traductor Lengua de Señas", // titulo
-        debugShowCheckedModeBanner: false,
-        initialRoute: "/onboarding", //vista principal
-        routes: <String, WidgetBuilder>{
-          "/onboarding": (BuildContext context) => new Onboarding(),
-          "/home": (BuildContext context) => new Home(),
-          "/components": (BuildContext context) => new Components(),
-          "/profile": (BuildContext context) => new Profile(),
-          "/settings": (BuildContext context) => new Settings(),
-          "/signin": (BuildContext context) => new Login(),
-          "/camara": (BuildContext context) => new Camara(),
-          "/historial": (BuildContext context) => new HistorialView(),
-          "/crearCuenta": (BuildContext context) => new CrearCuenta()
-        });
-  }
-}
-class AuthPage extends StatefulWidget {
-  const AuthPage({super.key});
-
-  @override
-  State<AuthPage> createState() => _AuthPageState();
-}
-
-class _AuthPageState extends State<AuthPage> {
-  final GoogleSignIn _googleSignIn = GoogleSignIn(); // Instancia de GoogleSignIn
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((account) {
-      if (account != null) {
-        // Usuario autenticado
-        Navigator.pushReplacementNamed(context, "/home"); // Redirigir a Home
-      }
-    });
-    _googleSignIn.signInSilently(); // Intentar iniciar sesión si hay sesión guardada
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: _googleSignIn.onCurrentUserChanged,
+      title: "Traductor Lengua de Señas",
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder<User?>(
+        stream: _auth.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  Theme.of(context).primaryColor,
-                ),
-              ),
-            );
+            return const Center(child: CircularProgressIndicator());
           }
-          if (_googleSignIn.currentUser != null) {
-            return Home(); // Si ya está autenticado
-          } else {
-            return Login(); // Si no está autenticado
+          
+          // Si el usuario no está autenticado, mostrar onboarding
+          if (!snapshot.hasData) {
+            return Onboarding();
           }
+          
+          // Si el usuario está autenticado, mostrar home
+          return Home();
         },
       ),
+      routes: <String, WidgetBuilder>{
+        "/onboarding": (BuildContext context) => Onboarding(),
+        "/home": (BuildContext context) => Home(),
+        "/components": (BuildContext context) => Components(),
+        "/profile": (BuildContext context) => Profile(),
+        "/settings": (BuildContext context) => Settings(),
+        "/signin": (BuildContext context) => Login(),
+        "/camara": (BuildContext context) => Camara(),
+        "/historial": (BuildContext context) => HistorialView(),
+        "/crearCuenta": (BuildContext context) => CrearCuenta()
+      },
     );
+  }
+}
+
+class AuthService {
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  static final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  static Future<User?> signInWithGoogle(BuildContext context) async {
+    try {
+      if (kIsWeb) {
+        GoogleAuthProvider authProvider = GoogleAuthProvider();
+        final UserCredential userCredential = 
+            await _auth.signInWithPopup(authProvider);
+        return userCredential.user;
+      } else {
+        final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+        if (googleUser == null) return null;
+
+        final GoogleSignInAuthentication googleAuth = 
+            await googleUser.authentication;
+        
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+
+        final UserCredential userCredential = 
+            await _auth.signInWithCredential(credential);
+            
+        return userCredential.user;
+      }
+    } catch (e) {
+      print("Error en el inicio de sesión con Google: $e");
+      // Aquí podrías mostrar un snackbar o diálogo de error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al iniciar sesión: $e')),
+      );
+      return null;
+    }
+  }
+
+  static Future<void> signOut() async {
+    await _auth.signOut();
+    if (!kIsWeb) {
+      await _googleSignIn.signOut();
+    }
   }
 }
